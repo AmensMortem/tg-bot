f4d9f1ce9f2b0560ad2a72f7879b482b819da4c3
AmensMortem <74256695+AmensMortem@users.noreply.github.com>
2021-04-19 20:09:43 +0300

    weather/world time

--- main.py
+++ main.py
@@ -3,20 +3,43 @@ from telegram.ext import CallbackContext, CommandHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import datetime
 import time
+from datetime import time
 from dotenv import load_dotenv
 import os
 import sqlite3
+import vk_api
+import random
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import pyowm
+from bs4 import BeautifulSoup
+import requests
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
+TOKEN_VK = os.getenv("TOKEN_VK")
+id_group_vk = os.getenv("id_group_vk")
+owm = pyowm.OWM("77747d4628e230296e6b495f66c3b817")
+
+keyboard = [["help", "weather"],
+            ["world_time", "закрыть клавиатуру"]]
+markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=False)
+dice_keyboard = [["30 секунд", "1 минута"],
+                 ["5 минут", "закрыть клавиатуру"]]
+markup_dice = ReplyKeyboardMarkup(dice_keyboard, one_time_keyboard=False)
 
 
 class DataBase:
     def __init__(self, id_tg, id_chat):
-        self.db = sqlite3.connect('datetime.db')
+        self.db = sqlite3.connect("datetime.db")
         self.cur = self.db.cursor()
         self.id_tg = id_tg
         self.id_chat = id_chat
+
+    def vk_add(self, id_on_vk):
+        self.cur.execute(
+            """INSERT INTO user(id_tg, id_chat) VALUES (?,?)""", (self.id_tg, self.id_chat)).fetchall()
+        self.db.commit()
+        self.db.close()
 
     def addition(self, date, reminding):
         self.cur.execute(
@@ -39,7 +62,7 @@ class DataBase:
         except:
             return 'error'
 
-    def editing(self, date_time):
+    def editing(self, date_time, reminding):
         res = self.cur.execute(
             """UPDATE имя_таблицы SET название_колонки = новое_значение WHERE условие""",
             (self.id_tg, date_time)).fetchall()
@@ -54,14 +77,19 @@ class DataBase:
 
 
 def start(update, context):
-    update.message.reply_text("Я бот time-manager, если ты хочешь узнать какие у меня есть команды напиши '/help'")
+    update.message.reply_text(
+        "Я бот time-manager, если ты хочешь узнать какие у меня есть команды напиши '/help'",
+        reply_markup=dice_keyboard)
 
 
 def help_(update, context):
     update.message.reply_text("/now, сегодняшняя дата, время до секунды\n"
                               "/timer ? секунд/минут/часов\n"
                               "/dtimer, удаление установленого таймера\n"
-                              "/ctimer, редактирование установленого таймера")
+                              "/ctimer, редактирование установленого таймера"
+                              "/weather, погода\n"
+                              "/world_time, мировое время\n"
+                              )
 
 
 def now(update, context):
@@ -129,10 +157,13 @@ def change_timer(update, context):
         if 'job' in context.chat_data:
             old_job = context.chat_data['job']
             old_job.schedule_removal()
-        if timestamp == 'с':
+        if timestamp == messages:
             new_job = context.job_queue.run_once(task, due, context=chat_id)
             context.chat_data['job'] = new_job
             update.message.reply_text(f'Отлично! Вернусь через {text_message[1]} {units_text}')
+        elif timestamp == messages:
+            pass
+
     except:
         update.message.reply_text('Опять что-то не так, я хочу спать вообще-то')
 
@@ -146,38 +177,100 @@ def addition(update, context):
     try:
         text_message = update.message.text.split(', ')
         date_, msg = text_message.split(',')
-        date = datetime.datetime.strptime(date_, '%Y-%m-%d %H:%M:%S')
+        date_, time_ = date_.split()
+        d, m, y = map(int, date_.split('.'))
+        hour, minutes = map(int, time_.split(':'))
+        new_date = datetime.datetime(year=2000 + y, month=m, day=d, hour=hour, minute=minutes)
         today = datetime.datetime.now()
-        delta = today - date
-        DataBase.addition(update.message.chat_id, date, msg)
+        delta = today - new_date
+        DataBase.addition(update.message.chat_id, new_date, msg)
+        timer_remind(delta)
+        task(context, reminded=msg)
+        print(new_date, msg)
+    except Exception:
+        print(Exception.__class__)
+
+
+def editing(update, context):
+    try:
+        text_message = update.message.text.split(', ')
+        date_, msg = text_message.split(',')
+        date_, time_ = date_.split()
+        d, m, y = map(int, date_.split('.'))
+        hour, minutes = map(int, time_.split(':'))
+        new_date = datetime.datetime(year=2000 + y, month=m, day=d, hour=hour, minute=minutes)
+        DataBase.editing(update.message.chat_id, new_date, msg)
+        task(context, reminded=msg)
+    except Exception:
+        print(Exception.__class__)
+
+
+def viewing(update, context):
+    try:
+        text_message = update.message.text.split(', ')
+        date_, msg = text_message.split(',')
+        date_, time_ = date_.split()
+        d, m, y = map(int, date_.split('.'))
+        hour, minutes = map(int, time_.split(':'))
+        new_date = datetime.datetime(year=2000 + y, month=m, day=d, hour=hour, minute=minutes)
+        today = datetime.datetime.now()
+        delta = today - new_date
+        DataBase.viewing(update.message.chat_id, new_date, msg)
         timer_remind(delta)
         task(context, reminded=msg)
     except Exception:
         print(Exception.__class__)
 
 
-def editing(update, context):
-    text_message = update.message.text.split(' ')
-    text_message = text_message[1:]
-
-
-#    DataBase.editing()
-
-
-def viewing(update, context):
-    text_message = update.message.text.split(' ')
-    text_message = text_message[1:]
-
-
-#    DataBase.viewing()
-
-
 def deletion(update, context):
-    text_message = update.message.text.split(' ')
-    text_message = text_message[1:]
+    try:
+        text_message = update.message.text.split(', ')
+        date_, msg = text_message.split(',')
+        date_, time_ = date_.split()
+        d, m, y = map(int, date_.split('.'))
+        hour, minutes = map(int, time_.split(':'))
+        new_date = datetime.datetime(year=2000 + y, month=m, day=d, hour=hour, minute=minutes)
+        today = datetime.datetime.now()
+        delta = today - new_date
+        DataBase.deletion(update.message.chat_id, new_date, msg)
+        timer_remind(delta)
+        task(context, reminded=msg)
+    except Exception:
+        print(Exception.__class__)
 
 
-#    DataBase.deletion()
+def add_vk(update, context):
+    try:
+        text_message = update.message.text.split(', ')
+        DataBase.vk_add(id_on_vk=text_message[1])
+    except Exception:
+        print(Exception.__class__)
+
+
+def weather(update, context):
+    try:
+        text_message = update.message.text.split(' ')
+        place = (text_message[1])
+        mgr = owm.weather_manager()
+        observation = mgr.weather_at_place(place)
+        w = observation.weather
+        temp = w.temperature("celsius")["temp"]
+        forecast = f"В городе {place} и {w.detailed_status}, а температура {temp}°C"
+        update.message.reply_text(forecast)
+    except:
+        update.message.reply_text("Какой странный город, попробуй еще раз")
+
+
+def world_time_def(update, context):
+    page = requests.get('https://www.google.com/search?q=Greenwich+Mean+Time&client=firefox-b-d&sxsrf=ALeKk03HNraW'
+                        'LezxwWl3A0QJCXtwM6XG5g%3A1618849508829&ei=5K59YNqJMuXirgTuwZrwBQ&oq=Greenwich+Mean+Time&gs'
+                        '_lcp=Cgdnd3Mtd2l6EAMyBAgAEEMyBQgAEMsBMgUIABDLATIECAAQQzIFCAAQyQMyAggAMgIIADICCAAyAggAMgIIA'
+                        'FCkLlikLmCPNWgAcAJ4AIABVIgBmgGSAQEymAEAoAECoAEBqgEHZ3dzLXdpesABAQ&sclient=gws-wiz&ved=0ahU'
+                        'KEwjai6yv3IrwAhVlsYsKHe6gBl4Q4dUDCA0&uact=5')
+    soup = BeautifulSoup(page.text, "html.parser")
+    parsed_time = soup.find_all('div', {'class': 'BNeawe iBp4i AP7Wnd'})[1].find_all(text=True, recursive=True)
+    world_time = f"По гринвичу время {parsed_time[0]}"
+    update.message.reply_text(world_time)
 
 
 def main():
@@ -187,8 +280,11 @@ def main():
     dp.add_handler(CommandHandler("delete", deletion))
     dp.add_handler(CommandHandler("edit", editing))
     dp.add_handler(CommandHandler("viewing", viewing))
+    dp.add_handler(CommandHandler("add_vk", add_vk))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_))
+    dp.add_handler(CommandHandler("world_time", world_time_def))
+    dp.add_handler(CommandHandler("weather", weather))
     dp.add_handler(CommandHandler("now", now))
     dp.add_handler(CommandHandler("timer", timer))
     dp.add_handler(CommandHandler("dtimer", unset_timer))
